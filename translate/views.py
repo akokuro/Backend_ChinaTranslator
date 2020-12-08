a1615fedf0d312f03012da85dd72481fabbea29e
Kate <aen@mstagency.ru>
2020-12-09 00:54:27 +0400

    check authantificate

--- translate/views.py
+++ translate/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -11,10 +11,14 @@ from bs4 import BeautifulSoup
 
 
 class ParserAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
+    permission_classes = [AllowAny]
 
     def post(self, request):
+        if not JWTAuthentication().authenticate(request):
+            return Response(
+                "Пользователь не авторизован",
+                401
+            )
         try:
             bkrs = request.query_params["bkrs"]
             if bkrs == "True":
